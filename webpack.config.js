5647c4d60dc9e4212787995a50cbedfaf4bce4b3
nwaamadioha <udokwu.temple960@gmail.com>
2022-06-15 22:27:36 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    output: {
+        clean: false,
+        path: path.join(__dirname, "/dist"),
+        filename: "index.bundle.js",
+    },
+    devServer: {
+        port: 3010,
+        static: {
+            directory: path.join(__dirname, "app")
+        },
+        open: true,
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ],
+            }
+        ]
+    },
+    plugins: [new MiniCssExtractPlugin()],
+};
